8aa564efc6b0cb7035f252efba5f81c7a7f77f99
Moaaz Belal <17829962+ezzmoaz@users.noreply.github.com>
2025-11-23 19:36:06 +0000

    Project creation

---
+++ LaRedouteRaceApp/LaRedouteRaceApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LaRedouteRaceApp
+//
+//  Created by Moaz Ezz on 23/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LaRedouteRaceApp/LaRedouteRaceApp/LaRedouteRaceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaRedouteRaceAppApp.swift
+//  LaRedouteRaceApp
+//
+//  Created by Moaz Ezz on 23/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct LaRedouteRaceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
